bba18e906fa54f0a06529531132b0637f810ffed
Maxime Langevin <Maxime Langevin>
2017-03-06 22:26:46 +0100

    force particule

--- src/Particule.java
+++ src/Particule.java
@@ -3,21 +3,24 @@ public class Particule {
     public double y;
     public double vx;
     public double vy;
+    public Force[] force;
+   
     
-    public Particule (double x, double y, double vx, double vy){
+    public Particule (double x, double y, double vx, double vy, Force[] force){
     	this.x=x;
     	this.y=y;
     	this.vx=vx;
     	this.vy=vy;
+    	this.force=force;
     }
     
     public double distance (Particule P1, Particule P2){
     	//calcul de la distance entre deux particules
     	return (Math.sqrt(Math.pow((P1.x-P2.x),2) +Math.pow((P1.y-P2.y),2)));
     }
-    public double force (Particule P1, Particule P2) {
+    public Force force (Particule P1, Particule P2) {
     	//calcul de la force entre deux particules, gravitationelle par d�faut
-    	return 0;
+    	return new Force(0,0);
     	
     }
 
